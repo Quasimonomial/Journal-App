184eadca16f184112fd048555cf9c6828997e377
Travis Herrick <Quasimonomial@gmail.com>
2014-11-19 11:37:46 -0800

    Added basic routing and root/posts controllers/model

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,18 @@
+class PostsController < ApplicationController
+  def index
+    @index = {}
+    render @index
+  end
+  
+  def create
+  end
+  
+  def update
+  end
+  
+  def show
+  end
+  
+  def destroy
+  end
+end
